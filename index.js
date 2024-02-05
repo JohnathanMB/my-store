e23fd20d56f77923d63bdd7a515ea148a61a7b96
JohnathanMB <johnathan.monsalve@udea.edu.co>
2024-02-04 21:44:33 -0500

    ejemplo de query params, uso de faker y evitar errores por orden

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const { faker } = require('@faker-js/faker')
 
 app.get('/', (req, res) => {
   res.send('Hello World');
@@ -13,17 +14,25 @@ app.get('/nueva-ruta', (req, res) => {
 })
 
 app.get('/products', (req, res) => {
-  res.json([
-    {
-      name: 'Product 1',
-      price: 1000
-    },
-    {
-      name: 'Product 2',
-      price: 2000
-    }
-  ])
+
+  const products = [];
+  const { size=10 } = req.query;
+
+  for( let index=0; index < size; index++){
+    products.push({
+      id: index,
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.url()
+    });
+  }
+
+  res.json(products)
 });
+
+app.get('/products/filter', (req, res) => {
+  res.send('Yo soy un filtro')
+})
 
 app.get('/products/:id', (req, res) =>{
   const { id } = req.params;
@@ -41,6 +50,21 @@ app.get('/categories/:categoryId/products/:productId', (req, res)=>{
     productId
   });
 });
+
+app.get('/users', (req, res) => {
+  const { limit, offset } = req.query;
+  if( limit && offset){
+    res.json({
+      limit,
+      offset
+    });
+  }else{
+    res.json({
+      message: "No hay parametros"
+    })
+  }
+
+})
 
 app.listen(port, () => {
   console.log('Port: '+ port)
