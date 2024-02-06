32f2901ee769240bb4239a75cbb1461016ac3c7b
JohnathanMB <johnathan.monsalve@udea.edu.co>
2024-02-06 10:04:32 -0500

    categories router

--- index.js
+++ index.js
@@ -15,16 +15,6 @@ app.get('/nueva-ruta', (req, res) => {
   })
 })
 
-app.get('/categories/:categoryId/products/:productId', (req, res)=>{
-  const { categoryId, productId } = req.params;
-  res.json({
-    categoryId,
-    productId
-  });
-});
-
-
-
 app.listen(port, () => {
   console.log('Port: '+ port)
 });
