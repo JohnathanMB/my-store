3149a53e506866e68e7a04b5f9b2d400e93775ab
Johnathan Arley Monsalve Bello <johnathan.monsalve@sofka.local>
2024-02-08 15:48:03 -0500

    apply cors options

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const express = require('express');
+const cosrs = require('cors');
+
 const app = express();
 const port = 3000;
 const routerApi = require('./routes')
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler')
 
 app.use(express.json());
+
+const whitlist = [
+  'http://localhost:8080',
+  'https://specific-domain.co',
+  'https://otro-specific-domain.co'
+];
+
+const options = {
+  origin: (origin, callback) => {
+    if(whitlist.includes(origin)) {
+      callback(null, true);
+    }else{
+      callback(new Error('No permitido'))
+    }
+  }
+}
+
+app.use(cosrs(options))
 
 app.get('/', (req, res) => {
   res.send('Hello World');
